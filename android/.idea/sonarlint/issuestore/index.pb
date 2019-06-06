2c51ac802c78175e1fa710463b6d46e97c9af74b
Ronaldo <venr93@sg.uob>
2019-06-06 13:25:00 +0800

    initial commit

---
+++ android/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+G
+flutter_app_android.iml,3/7/37fa337d9c7dee01be3c7acaa3683f1a69a9fb5f
+@
+app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
+<
+build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <device_info/DeviceInfoPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTDeviceInfoPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTDeviceInfoPlugin"]];
+}
+
+@end
